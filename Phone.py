922f12c0b0f260130e25babfe1f8050ff8e31502
James Bubb <jpbubb82@gmail.com>
2016-09-22 14:31:40 +0100

    Created initial tests and phone class

---
+++ Phone.py
@@ -0,0 +1,8 @@
+class Phone():
+
+    def __init__(self):
+        # Initialise the class
+        self.isOn = False
+
+    def switchOn(self):
+        self.isOn = True
---
+++ PhoneTests.py
@@ -0,0 +1,15 @@
+import unittest
+from Phone import Phone
+
+class PhoneTests(unittest.TestCase):
+
+    def setUp(self):
+        self.Phone = Phone()
+
+    def tearDown(self):
+        self.Phone = None
+
+    def test_phoneSwitchesOn(self):
+        self.assertFalse(self.Phone.isOn)
+        self.Phone.switchOn()
+        self.assertTrue(self.Phone.isOn)
